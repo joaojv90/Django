7c242ef6bb2beaee03d9b28a0353283c8144b84c
joaojv90 <joao.jaramillo@gmail.com>
2022-11-09 10:46:11 -0500

    create file base.html

--- webenterprise/core/urls.py
+++ webenterprise/core/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('services/', views.services, name='services'),
     path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),    
     path('store/', views.store, name='store'),
     path('contact/', views.contact, name='contact'),
     path('blog/', views.blog, name='blog'),
