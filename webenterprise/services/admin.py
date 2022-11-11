49d1d2f2bdba45e3a6029b28ac11c57a4ae9ecda
joaojv90 <joao.jaramillo@gmail.com>
2022-11-11 10:54:30 -0500

    changes

--- webenterprise/services/admin.py
+++ webenterprise/services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated') #De esta manera se muestran los campos ocultos
+
+admin.site.register(Service, ServiceAdmin) # Aumentar aquí toda configuración extendida
